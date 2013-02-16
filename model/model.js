0d30ac21b33d65612bca731564c1e8caa18bafd4
Germán Toro del Valle <gtorodelvalle@gmail.com>
2013-02-16 15:32:07 -0500

    First version of the app model.

---
+++ model/model.js
@@ -0,0 +1,49 @@
+var userCollection = Meteor.Collection("users");
+/*
+var userCollection = [
+  {
+    _id: user_id,
+    name: "User name",
+    username: "User username",
+    about: "User about",
+    avatarURL: "User avatar URL",
+    avatar: "data:image/png;base64,iVB...",
+    apps: [app1._id, app2._id, app3._id, ...],
+    appsSubmitted: [app4._id, app5._id, app6._id, ...],
+    liked: [user1._id, user2._id, user3._id, ...],
+    subscribers: [user1._id, user2._id, user3._id, ...]
+  }
+];
+*/
+
+var appCollection = Meteor.Collection("apps");
+/*
+var appCollection = [
+  {
+    _id: app_id,
+    name: "App name",
+    author: "App author",
+    category: "App category",
+    url: "App URL",
+    screenshot: "data:image/png;base64,iVB...",
+    submitter: userN._id,
+    liked: [user1._id, user2._id, user3._id, ...],
+    subscribers: [user1._id, user2._id, user3._id, ...]
+  }
+];
+*/
+
+var messageCollection = Meteor.Collection("messages");
+/*
+var messageCollection = [
+  {
+    _id: message_id,
+    type: "app | user",
+    to: appN._id | userN_id,
+    from: userN._id,
+    text: "The message text."
+    timestamp: "the date and time the message was sent",
+    read: [user1._id, user2._id, user3._id, ...]
+  }
+];
+*/
